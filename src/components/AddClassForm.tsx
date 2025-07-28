d47b9e401de0f85a278e45cdd7b0e2b936008ae8
Beck Orion <beckorion@gmail.com>
2025-07-27 18:15:40 -0700

    swap places of the add class and cancel buttons

--- src/components/AddClassForm.tsx
+++ src/components/AddClassForm.tsx
@@ -88,17 +88,17 @@ export default function AddClassForm({ onSubmit, onCancel }: AddClassFormProps) 
       </div>
       <div className="flex space-x-3">
         <button
-          type="submit"
-          className="flex-1 bg-blue-500 text-white p-3 rounded-lg font-medium hover:bg-blue-600 transition-colors"
-        >
-          Add Class
-        </button>
-        <button
           type="button"
           onClick={onCancel}
           className="flex-1 bg-gray-300 text-gray-700 p-3 rounded-lg font-medium hover:bg-gray-400 transition-colors"
         >
           Cancel
+        </button>
+        <button
+          type="submit"
+          className="flex-1 bg-blue-500 text-white p-3 rounded-lg font-medium hover:bg-blue-600 transition-colors"
+        >
+          Add Class
         </button>
       </div>
     </form>
